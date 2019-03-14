b3210f514de6ba6ed0cae567433e0776c97e6624
fastTofast <1024472173@qq.com>
2019-03-14 18:48:40 +0800

    增加排序

--- jsutil.js
+++ jsutil.js
@@ -1,21 +1,20 @@
 // 防抖
-let debounce = function (fn, delay) {
+let debounce = function(fn, delay) {
   let timeout, _this, args;
-  return function () {
-    clearTimeout(timeout)
+  return function() {
+    clearTimeout(timeout);
     _this = this;
     args = arguments;
     timeout = setTimeout(() => {
       fn.apply(_this, args);
-    }, delay)
-  }
-}
-
+    }, delay);
+  };
+};
 
 // 节流,结合时间戳和定时器两种方式,出发立即执行，停止出发后再执行一次
-let th = function (fn, delay) {
+let th = function(fn, delay) {
   let _this, args, timeout, start;
-  return function () {
+  return function() {
     _this = this;
     args = arguments;
     let now = Date.now();
@@ -30,14 +29,14 @@ let th = function (fn, delay) {
     } else if (!timeout) {
       timeout = setTimeout(() => {
         fn.apply(_this, args);
-      }, delay)
+      }, delay);
     }
-  }
-}
+  };
+};
 
 // 浅拷
-let shalowCopy = function (obj) {
-  if (typeof obj !== 'object' || !obj) return obj;
+let shalowCopy = function(obj) {
+  if (typeof obj !== "object" || !obj) return obj;
   let res = Array.isArray(obj) ? [] : {};
   for (key in obj) {
     if (obj.hasOwbProperty(key)) {
@@ -45,45 +44,78 @@ let shalowCopy = function (obj) {
     }
   }
   return res;
-}
+};
 
 // 深拷贝,或者直接使用JSON.parse(),JSON.stringtify()
-let deepCopy = function (obj) {
-  if (typeof obj !== 'object' || !obj) return obj;
+let deepCopy = function(obj) {
+  if (typeof obj !== "object" || !obj) return obj;
   let res = Array.isArray(obj) ? [] : {};
   for (key in obj) {
     if (obj.hasOwbProperty(key)) {
-      if (typeof obj[key] === 'object') {
+      if (typeof obj[key] === "object") {
         res[key] = deepCopy(obj[key]);
       } else {
-        res[key] = obj[key]
+        res[key] = obj[key];
       }
     }
   }
-  return res
-}
+  return res;
+};
 
 // 金额格式化
-let toCurrency = function (str) {
+let toCurrency = function(str) {
   if (isNaN(str)) return str;
-  let arr = (str + '').split('.');
-  let intNum = arr[0].replace(/\d(?=(\d{3})+$)/g, '$&,');
-  return intNum + (arr[1] ? '.' + arr[1] : '');
-}
+  let arr = (str + "").split(".");
+  let intNum = arr[0].replace(/\d(?=(\d{3})+$)/g, "$&,");
+  return intNum + (arr[1] ? "." + arr[1] : "");
+};
 
 // 平铺多维数组
-let flatten = function (arr) {
-  if (Object.prototype.toString.call(arr) !== '[object Array]') {
+let flatten = function(arr) {
+  if (Object.prototype.toString.call(arr) !== "[object Array]") {
     return arr;
   }
-  return arr.reduce((a, b) => a.concat(Array.isArray(b) ? flatten(b) : b), [])
-}
+  return arr.reduce((a, b) => a.concat(Array.isArray(b) ? flatten(b) : b), []);
+};
 
 // 平铺到制定深度
-let flatAnyDepth = function (arr, depth) {
-  if (Object.prototype.toString.call(arr) !== '[object Array]') {
+let flatAnyDepth = function(arr, depth) {
+  if (Object.prototype.toString.call(arr) !== "[object Array]") {
     return arr;
   }
-  return depth != 1 ? arr.reduce((a, b) => a.concat(Array.isArray(b) ? flatAnyDepth(b) : b), [])
-    : arr.reduce((a, b) => { a.concat(b) }, [])
+  return depth != 1
+    ? arr.reduce((a, b) => a.concat(Array.isArray(b) ? flatAnyDepth(b) : b), [])
+    : arr.reduce((a, b) => {
+        a.concat(b);
+      }, []);
+};
+
+//冒泡排序
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let k = 0; k < i; k++) {
+      if (arr[i] < arr[k]) {
+        [arr[k], arr[i]] = [arr[i], arr[k]];
+      }
+    }
+  }
+  return arr;
+}
+
+// 二分法
+function twodivide(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  let rightArr = [];
+  let leftArr = [];
+  let base = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > base) {
+      rightArr.push(arr[i]);
+    } else {
+      leftArr.push(arr[i]);
+    }
+  }
+  return [].concat(twodivide(leftArr), [base], twodivide(rightArr));
 }
